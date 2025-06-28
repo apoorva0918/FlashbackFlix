cf507c4e1c16281e7fd27975f903dd62f0424bd7
Apoorva Raj <apoorvaaa81@gmail.com>
2025-06-28 13:57:09 +0530

    Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Movies from './pages/Movies'
 import MovieDetails from './pages/MovieDetails'
@@ -7,17 +8,22 @@ import SeatLayout from './pages/SeatLayout'
 import MyBookings from './pages/MyBookings'
 import Favorite from './pages/Favorite'
 import { Toaster } from 'react-hot-toast'
-import Navbar from './components/Navbar'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Layout from './pages/admin/Layout'
 import Dashboard from './pages/admin/Dashboard'
 import AddShows from './pages/admin/AddShows'
 import ListShows from './pages/admin/ListShows'
 import ListBookings from './pages/admin/ListBookings'
+import { useAppContext } from './context/AppContext'
+import { SignIn } from '@clerk/clerk-react'
+import Loading from './components/Loading'
 
 const App = () => {
 
-  const isAdminRoute= useLocation().pathname.startsWith('/admin')
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  const { user } = useAppContext()
+
   return (
     <>
       <Toaster />
@@ -28,15 +34,21 @@ const App = () => {
         <Route path='/movies/:id' element={<MovieDetails/>} />
         <Route path='/movies/:id/:date' element={<SeatLayout/>} />
         <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path='/loading/:nextUrl' element={<Loading/>} />
+
         <Route path='/favorite' element={<Favorite/>} />
-        <Route path='/admin/*' element={<Layout/>}>
+        <Route path='/admin/*' element={user ? <Layout/> : (
+          <div className='min-h-screen flex justify-center items-center'>
+            <SignIn fallbackRedirectUrl={'/admin'} />
+          </div>
+        )}>
           <Route index element={<Dashboard/>}/>
-          <Route path='add-shows' element={<AddShows/>}/>
-          <Route path='list-shows' element={<ListShows/>}/>
-          <Route path='list-bookings' element={<ListBookings/>}/>
+          <Route path="add-shows" element={<AddShows/>}/>
+          <Route path="list-shows" element={<ListShows/>}/>
+          <Route path="list-bookings" element={<ListBookings/>}/>
         </Route>
       </Routes>
-      {!isAdminRoute && <Footer/>}
+       {!isAdminRoute && <Footer />}
     </>
   )
 }
